b8760aa08c420ebb53c394ee80c1805b6117a933
alexey <alexs22221@gmail.com>
2022-12-20 21:22:17 +0300

    Django Admin complete

--- mysite/libAdmin/admin.py
+++ mysite/libAdmin/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
-
+from libAdmin.models import Publisher, Author, Book 
 # Register your models here.
+
+
+class BookInLine(admin.TabularInline):
+	model = Book
+	
+
+
+class PublisherAdmin(admin.ModelAdmin):
+	list_display = ['id','name', 'city']
+	list_filter = ['is_active', 'city']
+	inlines = [BookInLine]
+
+
+class AuthorAdmin(admin.ModelAdmin):
+	list_display = ['id', 'first_name', 'last_name']
+	search_fields = ['first_name', 'last_name', 'biography']
+
+
+
+class BookAdmin(admin.ModelAdmin):
+	actions = ['mark_as_published', 'mark_as_draft', 'mark_as_review']
+	
+	def mark_as_published(self, request, queryset):
+		queryset.update(status='p')
+	
+	def mark_as_draft(self, request, queryset):
+		queryset.update(status='d')
+	
+	def mark_as_review(self, request, queryset):
+		queryset.update(status='r')
+
+	mark_as_published.short_description ='Перевести в статус Опубликовано'
+	mark_as_draft.short_description ='Перевести в статус Черновик'
+	mark_as_review.short_description ='Перевести в статус Ревью'
+
+
+
+
+
+admin.site.register(Publisher, PublisherAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
--- mysite/libAdmin/models.py
+++ mysite/libAdmin/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Publisher(models.Model):
+	name = models.CharField(max_length=30)
+	genre = models.CharField(max_length= 50)
+	city = models.CharField(max_length=60)
+	country = models.CharField(max_length=50)
+	is_active = models.BooleanField()
+
+	def __str__(self) -> str:
+		return f'{self.id} {self.name} ({self.country} {self.city})'
+
+
+class Author(models.Model):
+	first_name = models.CharField(max_length=30)
+	last_name = models.CharField(max_length=40)
+	email = models.EmailField()
+	biography = models.TextField()
+
+	def __str__(self) -> str:
+		return f'{self.id} {self.first_name} {self.last_name}'
+
+
+class Book(models.Model):
+	STATUS_CHOICES = [
+		('d', 'Draft'),
+		('r', 'Review'),
+		('p', 'PUblished'),
+	]
+	title = models.CharField(max_length=100)
+	authors = models.ManyToManyField(Author)
+	publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE )
+	publication_data = models.DateField()
+	status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='d')
